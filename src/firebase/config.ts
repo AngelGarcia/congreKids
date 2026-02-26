361cfcd3465dabbc19a9c1fdaf17e0bd20c16331
Ángel García Logrosán <aglogrosan@gmail.com>
2026-02-26 08:24:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "congre-kids",
+  "appId": "1:558793565287:web:fb1ca7a5dbcf22d49560c2",
+  "apiKey": "AIzaSyAIKpgCR3-T6gqBMFMfo-fh-F1IWBx6H8Q",
+  "authDomain": "congre-kids.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "558793565287"
+};
