747034f1ebf336c7e2eba0acf637acdb09aabbe7
Ángel García Logrosán <aglogrosan@gmail.com>
2026-09-14 13:35:03 +0000

    donde añado ests variables? Variables de Firebase (necesarias para que n

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "congre-kids",
-  "appId": "1:558793565287:web:fb1ca7a5dbcf22d49560c2",
-  "apiKey": "AIzaSyAIKpgCR3-T6gqBMFMfo-fh-F1IWBx6H8Q",
-  "authDomain": "congre-kids.firebaseapp.com",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "congre-kids",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:558793565287:web:fb1ca7a5dbcf22d49560c2",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAIKpgCR3-T6gqBMFMfo-fh-F1IWBx6H8Q",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "congre-kids.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "558793565287"
 };
